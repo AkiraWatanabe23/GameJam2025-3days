27b825bbb5ea600862f4cb2828e8f912375cc89d
YamamiyaShota <153970961+YamamiyaShota@users.noreply.github.com>
2025-02-25 14:45:52 +0900

    [change] ジャンプできるようにした [add] タグにGroundを追加

--- Assets/Tamamiya/PlayerController.cs
+++ Assets/Tamamiya/PlayerController.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float[] pos = new float[3] { -5, 0, 5 }; //移動できる場所
     [SerializeField] private int index = 1;
+    [SerializeField] private float jumpForce = 5f;
     [SerializeField] private bool isMove = true;
+    [SerializeField] private bool isGround = true;
+    Rigidbody rb;
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         isMove = true;
         this.transform.position = new Vector3(pos[index], this.transform.position.y);
     }
@@ -18,6 +23,10 @@ public class PlayerController : MonoBehaviour
         if (isMove)
         {
             Move();
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
         }
     }
 
@@ -47,6 +56,17 @@ public class PlayerController : MonoBehaviour
         this.transform.position = new Vector3(pos[index], this.transform.position.y);
     }
 
+    /// <summary>
+    /// プレイヤーのジャンプ
+    /// </summary>
+    public void Jump()
+    {
+        if(isGround)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Obstacle"))
@@ -57,6 +77,22 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Monday"))
         {
             GameManager.Instance.GameOver();
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = false;
         }
     }
 }
