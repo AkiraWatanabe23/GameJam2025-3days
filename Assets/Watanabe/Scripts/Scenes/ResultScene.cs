d7c7750d147c9ebc692af839128e62127776789b
aki_ra <aki00sub@gmail.com>
2025-02-27 03:37:56 +0900

    ResultSceneデータ受け渡し、差し替え

--- Assets/Watanabe/Scripts/Scenes/ResultScene.cs
+++ Assets/Watanabe/Scripts/Scenes/ResultScene.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class ResultScene : MonoBehaviour
@@ -11,11 +12,25 @@ public class ResultScene : MonoBehaviour
     [SerializeField]
     private Text _resultTimeText = default;
     [SerializeField]
+    private Text _resultWeekText = default;
+
+    [SerializeField]
     private Text _rankingFirstText = default;
     [SerializeField]
     private Text _rankingSecondText = default;
     [SerializeField]
     private Text _rankingThirdText = default;
+
+    private readonly Dictionary<Week, string> _week = new()
+    {
+        { Week.Monday, "月曜日" },
+        { Week.Tuesday, "火曜日" },
+        { Week.Wednesday, "水曜日" },
+        { Week.Thursday, "木曜日" },
+        { Week.Friday, "金曜日" },
+        { Week.Saturday, "土曜日" },
+        { Week.Sunday, "日曜日" },
+    };
 
     private void Start()
     {
@@ -41,7 +56,9 @@ public class ResultScene : MonoBehaviour
         var instance = new Ranking();
         var ranking = instance.Initialize(resultTime);
 
-        _resultTimeText.text = resultTime.ToString("F2").Replace('.', ':');
+        _resultTimeText.text = ScoreManager.Instance.ResultScore.ToString("F2").Replace('.', ':');
+        _resultWeekText.text = _week[ScoreManager.Instance.ResultWeek];
+
         _rankingFirstText.text = ranking[0].ToString("F2").Replace('.', ':');
         _rankingSecondText.text = ranking[1].ToString("F2").Replace('.', ':');
         _rankingThirdText.text = ranking[2].ToString("F2").Replace('.', ':');
